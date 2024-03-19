f9ca87783f6ae98db48358daac6625d7aad3f188
fran8ctba <geraldo.fvinicius@gmail.com>
2024-03-19 14:45:42 -0300

    Ex19

---
+++ utilidades/Triangulo.java
@@ -0,0 +1,23 @@
+package utilidades;
+
+public class Triangulo {
+
+    public Integer lado1;
+    public Integer lado2;
+    public Integer lado3;
+
+    public static boolean formaTriangulo(Integer lado1, Integer lado2, Integer lado3) {
+        return (lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1);
+    }
+    
+    public static String tipoTriangulo(Integer lado1, Integer lado2, Integer lado3) {
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Equilátero";
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            return "Isóscele";
+        } else {
+            return "Escaleno";
+        }
+    }
+    
+}
